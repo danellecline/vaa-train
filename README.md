266ab0626b8fe2aa5034163bcbe7508510e64701
Danelle Cline <dcline@mbari.org>
2020-05-14 05:42:22 +0000

    added auth and all schema items

--- README.md
+++ README.md
@@ -40,4 +40,14 @@ mutation createAnnotations {
   ano1: createAnnotation(input: {concept: "Scotoplanes globosa", annotator: "DCline", top: 0, left: 0, width: 0 height:0 }) {id, concept, annotator }
   ano2: createAnnotation(input: {concept: "Cystechinus loveni", annotator: "DCline", top: 0, left: 0, width: 0 height:0 }) {id, concept, annotator }
 }
+
+query ConceptContainsScoto {
+  listAnnotations(filter: {concept: {contains: "Scotoplanes"} }) {
+    items {
+      id
+      concept
+      annotator
+    }
+  }
+}
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="concept" placeholder="Todo concept">
-    <input type="text" v-model="annotator" placeholder="Todo annotator">
-    <button v-on:click="createAnnotation">Create Todo</button>
-  </div>
-</template>
-<template>
-  <div id="app">
-    <h1>App</h1>
-     <div v-for="item in todos" :key="item.id">
+    <h1>Annotation App</h1>
+    <input type="text" v-model="concept" placeholder="Annotation concept">
+    <input type="text" v-model="annotator" placeholder="Annotation annotator">
+    <input type="top" v-model="top" placeholder="Annotation top">
+    <input type="left" v-model="left" placeholder="Annotation left">
+    <input type="width" v-model="width" placeholder="Annotation width">
+    <input type="height" v-model="height" placeholder="Annotation height">
+    <button v-on:click="createAnnotation">Create Annotation</button>
+    <div v-for="item in annotations" :key="item.id">
       <h3>{{ item.concept }}</h3>
       <p>{{ item.annotator }}</p>
+      <p>{{ item.top }}</p>
+      <p>{{ item.left }}</p>
+      <p>{{ item.width }}</p>
+      <p>{{ item.height }}</p>
     </div>
   </div>
 </template>
@@ -19,37 +22,47 @@
 <script>
 import { API } from 'aws-amplify';
 import { createAnnotation } from './graphql/mutations';
+import { listAnnotations } from './graphql/queries';
 
 export default {
-  concept: 'app',
+  name: 'App',
+  async created() {
+    this.getAnnotations();
+  },
   data() {
     return {
       concept: '',
-      annotator: ''
+      annotator: '',
+      top: 0.,
+      left: 0.,
+      width: 0.,
+      height: 0.,
+      annotations: []
     }
   },
   methods: {
     async createAnnotation() {
-      const { concept, annotator } = this;
+      const { concept, annotator, top, left, width, height } = this;
       if (!concept || !annotator) return;
-      const todo = { concept, annotator };
+      const annotation = { concept, annotator, top, left, width, height };
+      this.annotations = [...this.annotations, annotation];
       await API.graphql({
         query: createAnnotation,
-        variables: {input: todo},
+        variables: {input: annotation},
       });
+      this.top = 0.;
+      this.left = 0.;
+      this.width = 0.;
+      this.height = 0.;
       this.concept = '';
       this.annotator = '';
+    },
+    async getAnnotations() {
+      const annotations = await API.graphql({
+        query: listAnnotations
+      });
+      this.annotations = annotations.data.listAnnotations.items;
     }
   }
-};
-</script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
